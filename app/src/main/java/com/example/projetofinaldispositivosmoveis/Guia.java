87ec7b1210f72aa3d319d970a0bdaca9981e362f
Paula <paula.bicca@gmail.com>
2019-06-05 14:31:18 -0300

    Criação da classe Guia

---
+++ app/src/main/java/com/example/projetofinaldispositivosmoveis/Guia.java
@@ -0,0 +1,76 @@
+package com.example.projetofinaldispositivosmoveis;
+
+public class Guia {
+    private String nomeAnimal;
+    private String idadeAnimal;
+    private String pesoAnimal;
+    private String racaAnimal;
+    private String tipoAnimal;
+    private String observacaoAnimal;
+    private String telefoneDono;
+    private String donoNome;
+
+    public String getNomeAnimal() {
+        return nomeAnimal;
+    }
+
+    public void setNomeAnimal(String nomeAnimal) {
+        this.nomeAnimal = nomeAnimal;
+    }
+
+    public String getIdadeAnimal() {
+        return idadeAnimal;
+    }
+
+    public void setIdadeAnimal(String idadeAnimal) {
+        this.idadeAnimal = idadeAnimal;
+    }
+
+    public String getPesoAnimal() {
+        return pesoAnimal;
+    }
+
+    public void setPesoAnimal(String pesoAnimal) {
+        this.pesoAnimal = pesoAnimal;
+    }
+
+    public String getRacaAnimal() {
+        return racaAnimal;
+    }
+
+    public void setRacaAnimal(String racaAnimal) {
+        this.racaAnimal = racaAnimal;
+    }
+
+    public String getTipoAnimal() {
+        return tipoAnimal;
+    }
+
+    public void setTipoAnimal(String tipoAnimal) {
+        this.tipoAnimal = tipoAnimal;
+    }
+
+    public String getObservacaoAnimal() {
+        return observacaoAnimal;
+    }
+
+    public void setObservacaoAnimal(String observacaoAnimal) {
+        this.observacaoAnimal = observacaoAnimal;
+    }
+
+    public String getTelefoneDono() {
+        return telefoneDono;
+    }
+
+    public void setTelefoneDono(String telefoneDono) {
+        this.telefoneDono = telefoneDono;
+    }
+
+    public String getDonoNome() {
+        return donoNome;
+    }
+
+    public void setDonoNome(String donoNome) {
+        this.donoNome = donoNome;
+    }
+}
