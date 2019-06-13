be4234beb30ec5fab7341b54ac5bc9bf5cf1be5a
Awdren Ribeiro <awdrenribeiro@yahoo.com.br>
2019-06-12 21:28:48 -0300

    criação da tela de edição de guias e da classe de edição

---
+++ app/src/main/java/com/example/projetofinaldispositivosmoveis/EdicaoCadastro.java
@@ -0,0 +1,13 @@
+package com.example.projeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdicaoCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edicao_cadastro);
+    }
+}
