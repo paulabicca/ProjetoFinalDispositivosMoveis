d2aaf21ad388395809fd3758ea570eff5e262036
Leticia Soares de Oliveira Rosa <leticiamarschner@gmail.com>
2019-06-17 21:17:16 -0300

    Tentando implementar funcionalidade do swipe

--- app/src/main/java/com/example/projetofinaldispositivosmoveis/SwipeActivity.java
+++ app/src/main/java/com/example/projetofinaldispositivosmoveis/SwipeActivity.java
@@ -10,12 +10,19 @@ public class SwipeActivity extends AppCompatActivity {
 
     //layoutSwipe Linearlayout;
     private Button btnEscolher;
+    private String nomeAnimal;
+    private String tipoAnimal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
 
         btnEscolher = (Button) findViewById(R.id.btnEscolher);
+        nomeAnimal = (String) findViewById(R.id.nomeAnimal);
+        tipoAnimal = (String) findViewById(R.id.tipoAnimal);
+       // layout = LinearLayout findViewById(R.id.layout);
+
 
         btnEscolher.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +31,29 @@ public class SwipeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        n = nomeAnimal.lenght();
+        t= tipoAnimal.lenght();
+        contador=0;
+      //  layout.setOnTouchListener( new OnSwipeTouchListener(this){
+
+
+         //public void onSwipeRight(){
+          //  super.onSwipeRight();
+          //   for (contador = 0;contador< n;contador++){
+          //       for (contador=0;contador<t;contador++)
+           //  }
+//
+      //  }
+     //    public void onSwipeLeft(){
+         //    super.onSwipeLeft();
+           //  for (contador = 0;contador< n;contador++){
+             //    for (contador=0;contador<t;contador++)
+           //  }
+
+         }
+
+
     }
-}
+//}
+//}
