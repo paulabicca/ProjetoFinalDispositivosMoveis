0fe67243717f6d62e5342c2f43e82f0202ae6fea
Paula <paula.bicca@gmail.com>
2019-06-20 21:30:46 -0300

    Criação de novas páginas

---
+++ app/src/main/java/com/example/projetofinaldispositivosmoveis/AdapterGuia.java
@@ -0,0 +1,4 @@
+package com.example.projetofinaldispositivosmoveis;
+
+public class AdapterGuia {
+}
---
+++ app/src/main/java/com/example/projetofinaldispositivosmoveis/ListaGuias.java
@@ -0,0 +1,13 @@
+package com.example.projetofinaldispositivosmoveis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaGuias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_guias);
+    }
+}
